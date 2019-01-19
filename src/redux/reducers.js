64605ce936aae8950cd69f2b595e57441d36b497
Gabriel Macario <gmacarioweb@gmail.com>
2019-01-18 18:50:09 -0800

    Create basic reducers

---
+++ src/redux/reducers.js
@@ -0,0 +1,71 @@
+import { combineReducers } from 'redux';
+import {
+    SELECT_SUBREDDIT,
+    INVALIDATED_SUBREDDIT,
+    REQUEST_POSTS,
+    RECEIVE_POSTS,
+    INVALIDATE_SUBREDDIT
+} from './actions';
+
+function selectedSubreddit(state = 'reactjs', action) {
+    switch (action.type) {
+        case SELECT_SUBREDDIT:
+            return action.subbreddit;
+        default:
+            return state;
+    }
+}
+
+function posts(
+    state = {
+        isFetching: false,
+        didInvalidate: false,
+        items: []
+    },
+    action
+) {
+    switch(action.type) {
+        case INVALIDATED_SUBREDDIT:
+            return {
+                ...state,
+                didInvalidate: true
+            }
+        case REQUEST_POSTS:
+            return {
+                ...state,
+                isFetching: true,
+                didInvalidate: false
+            };
+        case RECEIVE_POSTS:
+            return {
+                ...state,
+                isFetching: false,
+                didInvalidate: false,
+                items: action.posts,
+                lastUpdated: action.receivedAt
+            }
+        default:
+            return state;
+    }
+}
+
+function postsBySubreddit(state = {}, action) {
+    switch(action.type) {
+        case INVALIDATE_SUBREDDIT:
+        case RECEIVE_POSTS:
+        case REQUEST_POSTS:
+            return {
+                ...state,
+                [action.subreddit]: posts(state[action.subreddit], action)
+            };
+        default:
+            return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    postsBySubreddit,
+    selectedSubreddit
+});
+
+export default rootReducer;
