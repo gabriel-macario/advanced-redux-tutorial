e4f997314bd8de16ea28268950f641ac8f74c1a3
Gabriel Macario <gmacarioweb@gmail.com>
2019-01-19 22:47:04 -0800

    Put Picker component in AsyncApp

--- src/containers/AsyncApp.js
+++ src/containers/AsyncApp.js
@@ -2,9 +2,14 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import {
+    selectSubreddit,
     fetchPostsIfNeeded,
+    invalidateSubreddit
 } from '../redux/actions';
 import Posts from '../components/Posts';
+import Picker from '../components/Picker';
+
+const pickerOptions = ['reactjs', 'frontend', 'learnprogramming']
 
 class AsyncApp extends React.Component {
     // constructor(props) {
@@ -12,15 +17,51 @@ class AsyncApp extends React.Component {
     // }
 
     componentDidMount() {
-        const { dispatch } = this.props;
-        dispatch(fetchPostsIfNeeded('learnprogramming'));
+        const { dispatch, selectedSubreddit } = this.props;
+        dispatch(fetchPostsIfNeeded(selectedSubreddit));
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.selectedSubreddit !== prevProps.selectedSubreddit) {
+            const { dispatch, selectedSubreddit } = this.props;
+            dispatch(fetchPostsIfNeeded(selectedSubreddit));
+        }
+    }
+
+    handlePickerChange = (nextSubreddit) => {
+        this.props.dispatch(selectSubreddit(nextSubreddit));
+        this.props.dispatch(fetchPostsIfNeeded(nextSubreddit));
+    }
+
+    handleRefreshClick = (event) => {
+        event.preventDefault();
+
+        const { dispatch, selectedSubreddit } = this.props;
+        dispatch(invalidateSubreddit(selectedSubreddit));
+        dispatch(fetchPostsIfNeeded(selectedSubreddit));
     }
 
     render() {
-        const { posts, isFetching, lastUpdated } = this.props;
+        const { posts, selectedSubreddit, isFetching, lastUpdated } = this.props;
 
         return (
             <div>
+                <Picker
+                    value={selectedSubreddit}
+                    onChange={this.handlePickerChange}
+                    options={pickerOptions} 
+                />
+                <p>
+                    {lastUpdated && (
+                        <span>
+                            last updated at {new Date(lastUpdated).toLocaleTimeString()}.{' '}
+                        </span>
+
+                    )}
+                    {!isFetching && (
+                        <button onClick={this.handleRefreshClick}>Refresh</button>
+                    )}
+                </p>
                 {isFetching && posts.length === 0 && <h2>Loading...</h2>}
                 {!isFetching && posts.length === 0 && <h2>Empty</h2>}
                 {posts.length > 0 && (
@@ -39,9 +80,8 @@ class AsyncApp extends React.Component {
 // }
 
 function mapStateToProps(state) {
-    const { postsBySubreddit } = state;
-    const { isFetching, lastUpdated, items: posts } = postsBySubreddit[
-        "learnprogramming"
+    const { postsBySubreddit, selectedSubreddit } = state;
+    const { isFetching, lastUpdated, items: posts } = postsBySubreddit[selectedSubreddit
     ] || {
             isFetching: true,
             items: []
@@ -50,7 +90,8 @@ function mapStateToProps(state) {
     return {
         posts,
         isFetching,
-        lastUpdated
+        lastUpdated,
+        selectedSubreddit
     }
 }
 
