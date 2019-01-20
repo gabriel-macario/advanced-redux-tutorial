43e4b1dfe033b50a8211b053f68d6c9c6f3acc0b
Gabriel Macario <gmacarioweb@gmail.com>
2019-01-19 20:34:09 -0800

    Render Root in index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
 import 'babel-polyfill';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { selectSubreddit, fetchPostsIfNeeded } from './redux/actions';
-import rootReducer from './redux/reducers';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Root from './containers/Root';
 
-const loggerMiddleware = createLogger();
-
-const store = createStore(
-    rootReducer,
-    applyMiddleware(
-        thunkMiddleware, // lets us dispatch() functions
-        loggerMiddleware // neat middleware that logs actions
-    )
-)
-
-store.dispatch(selectSubreddit('reactjs'))
-store.dispatch(fetchPostsIfNeeded('reactjs')).then(() => console.log(store.getState()))
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Root />, document.getElementById('root'))
