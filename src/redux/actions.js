b5180e48922a3d3fe29d28ec7e7af65f16c1c622
Gabriel Macario <gmacarioweb@gmail.com>
2019-01-19 16:44:00 -0800

    Implement fetchPosts actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,11 @@
+import fetch from 'cross-fetch';
+
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT';
 export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT';
 export const REQUEST_POSTS = 'REQUEST_POSTS';
 export const RECEIVE_POSTS = 'RECEIVE_POSTS';
 
+// Synchronous Actions
 export function selectSubreddit(subreddit) {
     return {
         type: SELECT_SUBREDDIT,
@@ -33,3 +36,41 @@ export function receivePosts(subreddit, json) {
     }
 }
 
+// Asynchronous Actions
+// Thunk action creator
+// Used like any other action creator:
+// store.dispatch(fetchPosts('reactjs'))
+
+export function fetchPosts(subreddit) {
+    // Thunk middleware knows how to handle functions.
+    // It passes the dispatch method as an argument to the function thus making it able to dispatch actions itself
+
+    return function (dispatch) {
+        // First dispatch: The app is updated to inform that the API call is starting.
+
+        dispatch(requestPosts(subreddit));
+
+        // The function called by the thunk middleware can return a value, that is passed on as the return value of the dispatch method.
+
+        // In this case, we return a promise to wait for.
+        // This is not required by thunk middlweware, but it is convenient for us.
+
+        return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+            .then(
+                response => response.json(),
+                // Do not use catch, because that will also catch
+                // any errors in the dispatch and resulting render,
+                // causing a loop of 'Unexpected batch number' errors.
+                // https://github.com/facebook/react/issues/6895
+                error => console.log('An error occurred.', error)
+            )
+            .then(json =>
+                // We can dispatch many times!
+                // Here, we update the app state with the resutls of the API call.
+
+                dispatch(receivePosts(subreddit, json))
+            )
+
+
+    }
+}
